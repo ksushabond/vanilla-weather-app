239e5db9c7b6e5e80a5b28e6e81a529263c0ac8c
ksushabond <114615946+ksushabond@users.noreply.github.com>
2022-10-12 14:04:33 +0200

    Changed max-width of weather-app-wrapper, added the background color to the body, cleaned my app.js

--- src/app.js
+++ src/app.js
@@ -71,11 +71,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-  console.log(apiUrl);
 }
 
 function displayTemperature(response) {
